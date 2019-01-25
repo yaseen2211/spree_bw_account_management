10ad030d93803520dab56d1ec8f3266b59feb04f
zain rafique <zainraf21@gmail.com>
2019-01-25 22:05:56 +0500

    Done with personal information

---
+++ app/models/spree/user_decorator.rb
@@ -0,0 +1,5 @@
+Spree::User.class_eval do
+  Spree::PermittedAttributes.user_attributes.push(:first_name, :last_name, :phone_number, :date_of_birth, :month_of_birth, :head, :left_shoulder, :right_shoulder, :left_arm)
+  validates :first_name, :last_name, :phone_number, :date_of_birth, :month_of_birth, :head, :left_shoulder, :right_shoulder, :left_arm, presence: true
+end
+
