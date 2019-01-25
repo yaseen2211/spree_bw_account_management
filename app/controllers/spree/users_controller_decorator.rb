43e13101ea3feb33f7cf15246f91813cf59a53e0
zain rafique <zainraf21@gmail.com>
2019-01-25 23:14:00 +0500

    make ajax base

---
+++ app/controllers/spree/users_controller_decorator.rb
@@ -0,0 +1,22 @@
+Spree::UsersController.class_eval do
+
+  def update
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        # flash[:success] = Spree.t(:account_updated)
+        # redirect_to edit_admin_user_path(@user)
+        format.html { redirect_to account_path, notice: Spree.t(:account_updated) }
+        format.js
+      else
+        format.html { render action: "edit" }
+        format.js
+      end
+    end
+  end
+
+end
