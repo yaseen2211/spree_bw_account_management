22f6f5ad9988d0678d2b373ee598aa0b9aac9272
yaseen iqbal <p136011@nu.edu.pk>
2019-02-06 12:34:11 +0500

    fix registration bug

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,6 +1,6 @@
 Spree::User.class_eval do
 	
-  validates :first_name, :last_name, :phone_number, :date_of_birth, :month_of_birth, :head, :left_shoulder, :right_shoulder, :left_arm, presence: true
+  validates :first_name, :last_name, :phone_number, :date_of_birth, :month_of_birth, :head, :left_shoulder, :right_shoulder, :left_arm, presence: true, on: :update
   
   has_many :addresses
 end
