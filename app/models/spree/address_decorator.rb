14df3fe236eda123cc4bad3b9411ec9c6440d728
Shahbaz Hassan <yaseeniqbal119@gmail.com>
2019-02-07 21:17:42 +0500

    fix nill error

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -4,13 +4,13 @@ Spree::Address.class_eval do
 	  callback.raw_filter.attributes.reject! { |key| key == :lastname } if callback.raw_filter.respond_to?(:attributes)
 	end
 
-	validates :firstname, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-name').last.title }
-	validates :phone, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-number').last.title },
-                    :numericality => {:message => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-number-formate').last.title }
-	validates :address1, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-address-1').last.title }
-	validates :city, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-town').last.title }
-	validates :state, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-state').last.title }
-	validates :country, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-country').last.title }
+	validates :firstname, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-name').last.try(:title) }
+	validates :phone, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-number').last.try(:title) },
+                    :numericality => {:message => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-contact-number-formate').last.try(:title) }
+	validates :address1, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-address-1').last.try(:title) }
+	validates :city, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-town').last.try(:title) }
+	validates :state, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-state').last.try(:title) }
+	validates :country, :presence => {:message     => Spree::PageContent.from_gen_slug(I18n.locale,'address-error-msg').spec_slug('address-error-msg-country').last.try(:title) }
 
 	def require_zipcode?
 	  false
