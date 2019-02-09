5bdfebc4b38441daabc451737bc508c8dbc4ade7
Shahbaz Hassan <yaseeniqbal119@gmail.com>
2019-02-09 20:49:22 +0500

    comment validations

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,20 +1,20 @@
 Spree::Address.class_eval do
-	_validators.reject! { |key, _| key == :lastname }
-	_validate_callbacks.each do |callback|
-	  callback.raw_filter.attributes.reject! { |key| key == :lastname } if callback.raw_filter.respond_to?(:attributes)
-	end
+	# _validators.reject! { |key, _| key == :lastname }
+	# _validate_callbacks.each do |callback|
+	#   callback.raw_filter.attributes.reject! { |key| key == :lastname } if callback.raw_filter.respond_to?(:attributes)
+	# end
 
-	validates :firstname, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-name').last.try(:title) }
-	validates :phone, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-number').last.try(:title) },
-                    :numericality => {:message => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-number-formate').last.try(:title) }
-	validates :address1, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-address-1').last.try(:title) }
-	validates :city, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-town').last.try(:title) }
-	validates :state, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-state').last.try(:title) }
-	validates :country, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-country').last.try(:title) }
+	# validates :firstname, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-name').last.try(:title) }
+	# validates :phone, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-number').last.try(:title) },
+ #                    :numericality => {:message => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-contact-number-formate').last.try(:title) }
+	# validates :address1, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-address-1').last.try(:title) }
+	# validates :city, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-town').last.try(:title) }
+	# validates :state, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-state').last.try(:title) }
+	# validates :country, :presence => {:message     => Spree::PageContent.from_gen_slug('address-error-msg').spec_slug('address-error-msg-country').last.try(:title) }
 
-	def require_zipcode?
-	  false
-	end
+	# def require_zipcode?
+	#   false
+	# end
 
   belongs_to :user
 end
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,16 +1,15 @@
 Spree::User.class_eval do
 
 
-  validates :first_name, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-first-name').last.try(:title) }, on: :update
-  validates :last_name, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-last-name').last.try(:title) }, on: :update
-	validates :phone_number, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-phone-number').last.try(:title) },
-	                :numericality => {:message => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-phone-number-formate').last.try(:title) }, on: :update
-  validates :email, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-email').last.try(:title) }, on: :create
-  validates_format_of  :email,:message =>Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-email-formate').last.try(:title),  :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-	validates :left_shoulder, presence: {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-msr-shld-lft-formate').last.try(:title) }, format: { with: /\A\d+(?:\.\d{2})?\z/ }, on: :update
-	validates :date_of_birth, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-birth-date').last.try(:title) }, on: :update
-  validates :month_of_birth, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-birth-month').last.try(:title) }, on: :update
-
+ #  validates :first_name, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-first-name').last.try(:title) }, on: :update
+ #  validates :last_name, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-last-name').last.try(:title) }, on: :update
+	# validates :phone_number, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-phone-number').last.try(:title) },
+	#                 :numericality => {:message => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-phone-number-formate').last.try(:title) }, on: :update
+ #  validates :email, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-email').last.try(:title) }, on: :create
+ #  validates_format_of  :email,:message =>Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-email-formate').last.try(:title),  :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+	# validates :left_shoulder, presence: {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-msr-shld-lft-formate').last.try(:title) }, format: { with: /\A\d+(?:\.\d{2})?\z/ }, on: :update
+	# validates :date_of_birth, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-birth-date').last.try(:title) }, on: :update
+ #  validates :month_of_birth, :presence => {:message     => Spree::PageContent.from_gen_slug('user-error-msg').spec_slug('user-error-msg-birth-month').last.try(:title) }, on: :update
 
   has_many :addresses
 end
