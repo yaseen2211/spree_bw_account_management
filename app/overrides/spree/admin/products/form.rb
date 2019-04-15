b9b86ebe6622e442ba65c50d1fd95654f5473a30
yaseen iqbal <p136011@nu.edu.pk>
2019-04-15 21:00:15 +0500

    status fix on admin

--- app/overrides/spree/admin/products/form.rb
+++ app/overrides/spree/admin/products/form.rb
@@ -3,13 +3,15 @@ Deface::Override.new(
   name: 'Home Check To Product',
   insert_before: 'div[data-hook="admin_product_form_price"]',
   text: <<-HTML
-          <div data-hook="admin_product_form_to_home">
-            <%= f.field_container :to_home, class: ['form-group'] do %>
-              To Home
-              <%= f.error_message_on :to_home %>
-              <%= f.check_box :to_home, class: 'form-control' %>
-            <% end %>
-          </div>
+          <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
+            <div data-hook="admin_product_form_to_home">
+              <%= f.field_container :to_home, class: ['form-group'] do %>
+                To Home
+                <%= f.error_message_on :to_home %>
+                <%= f.check_box :to_home, class: 'form-control' %>
+              <% end %>
+            </div>
+          <% end %>
         HTML
 )
 
