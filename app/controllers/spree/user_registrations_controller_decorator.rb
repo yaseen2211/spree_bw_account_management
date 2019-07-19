af0116323375fdd662a5ddc0123608aac8e991a2
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-07-19 23:03:33 +0500

    Updated flash notice to error

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -11,7 +11,7 @@ Spree::UserRegistrationsController.class_eval do
         session[:spree_user_signup] = true
         respond_with resource, location: after_sign_up_path_for(resource)
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
+        set_flash_message :error, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
